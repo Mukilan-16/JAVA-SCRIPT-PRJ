037420dea5f0312c53b07e172b23bf6a595be464
MUKILAN <mukilan.atkavi@gmail.com>
2025-08-12 05:44:14 +0530

    Update product.js

--- P6/product.js
+++ P6/product.js
@@ -2,7 +2,7 @@ let productimg = document.getElementById("productimg");
 let btn = document.getElementsByClassName("btn");
 
 btn[0].onclick = function(){
-    productimg.src = "P6/image1.png";
+    productimg.src = "./image1.png";
 
     for(bt of btn){
         bt.classList.remove("active");
@@ -10,16 +10,17 @@ btn[0].onclick = function(){
     this.classList.add("active");
 }
 btn[1].onclick = function(){
-    productimg.src = "C:\PROJECTS JS\P6\image2.png";
+    productimg.src = "./image2.png";
     for(bt of btn){
         bt.classList.remove("active");
     }
     this.classList.add("active");
 }
 btn[2].onclick = function(){
-    productimg.src = "C:\PROJECTS JS\P6\image3.png";
+    productimg.src = "./image3.png";
     for(bt of btn){
         bt.classList.remove("active");
     }
     this.classList.add("active");
+
 }
